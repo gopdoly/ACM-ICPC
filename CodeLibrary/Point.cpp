8ad0eb2a102e4600513c8c0a787ae6282f38618a
ycl <gopdoly@163.com>
2014-05-30 13:37:59 +0800

    add point.cpp

---
+++ CodeLibrary/Point.cpp
@@ -0,0 +1,36 @@
+struct Point {
+    double x, y;
+
+    Point() {}
+    Point(double x, double y)
+    : x(x), y(y) {}
+
+    Point operator + (const Point& a) const {
+        return Point(x + a.x, y + a.y);
+    }
+    Point operator - (const Point& a) const {
+        return Point(x - a.x, y - a.y);
+    }
+    Point operator * (double k) const {
+        return Point(x * k, y * k);
+    }
+    Point operator / (double k) const {
+        return Point(x / k, y / k);
+    }
+
+    bool operator < (const Point &a) {
+        return x < a.x || (x == a.x && y < a.y);
+    }
+    bool operator == (const Point &a) {
+        return dcmp(x - a.x) == 0 && dcmp(y - a.y) == 0;
+    }
+};
+double dot(const Point &a, const Point &b) {
+    return a.x * b.x + a.y * b.y;
+}
+double cross(const Point &a, const Point &b) {
+    return a.x * b.y - a.y * b.x;
+}
+double length(const Point &a) {
+    return sqrt(dot(a, a));
+}
